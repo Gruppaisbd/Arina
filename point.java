aa72bfbe9400204af17a8e47788d614a9d1a5b14
Gruppaisbd <73101245+Gruppaisbd@users.noreply.github.com>
2020-11-03 19:37:49 +0300

    Add files via upload

---
+++ point.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Point{
+	public double x;
+	public double y;
+	
+	public Point(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	public void turn(double l, Point b){
+		double x,y;
+		l = Math.toRadians(l);
+		x = (this.x-b.x)*Math.cos(l) - (this.y-b.y)*Math.sin(l);
+		y = (this.x-b.x)*Math.sin(l) + (this.y-b.y)*Math.cos(l);
+		this.x = Math.round(x+b.x);
+		this.y = Math.round(y+b.y);
+	}
+}
